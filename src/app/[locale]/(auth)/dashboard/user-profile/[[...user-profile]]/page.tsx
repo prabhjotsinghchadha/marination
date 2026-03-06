42d95931f11698734501a71d9fa0d5f4df15c263
Prabhjot Singh Chadha <prabhjottechs@gmail.com>
2026-03-06 15:58:55 +0530

    Initial commit

---
+++ src/app/[locale]/(auth)/dashboard/user-profile/[[...user-profile]]/page.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { UserProfile } from '@clerk/nextjs';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { getI18nPath } from '@/utils/Helpers';
+
+type UserProfilePageProps = {
+  params: Promise<{ locale: string }>;
+};
+
+export async function generateMetadata(props: UserProfilePageProps): Promise<Metadata> {
+  const { locale } = await props.params;
+  const t = await getTranslations({
+    locale,
+    namespace: 'UserProfile',
+  });
+
+  return {
+    title: t('meta_title'),
+  };
+}
+
+export default async function UserProfilePage(props: UserProfilePageProps) {
+  const { locale } = await props.params;
+  setRequestLocale(locale);
+
+  return (
+    <div className="my-6 lg:-ml-12">
+      <UserProfile
+        path={getI18nPath('/dashboard/user-profile', locale)}
+      />
+    </div>
+  );
+};
